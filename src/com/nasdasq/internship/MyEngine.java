43d71851a506b246fad9b7fe5da7249ded3a5467
Eugis <eugenijus.sersniovas@gmail.com>
2018-03-31 15:38:28 +0300

    working matching for both buy and sell orders

--- src/com/nasdasq/internship/MyEngine.java
+++ src/com/nasdasq/internship/MyEngine.java
@@ -15,137 +15,91 @@ class MyEngine implements MatchingEngine {
         List<Trade> trades = new ArrayList<>();
         //Place your implementation here
 
-        if(orderNew.getSide() == Side.SELL){
-            //trades = makeTrades(trades,orderNew,buyOrders);
-            sellOrders.add(orderNew);
-        }
-        else {
+        if(orderNew.getSide() == Side.SELL)
+            trades = makeTrades(trades,orderNew,buyOrders);
+        else
             trades = makeTrades(trades,orderNew,sellOrders);
-
-//            Order bestSellOrder;
-//            while(orderNew.getQuantity().compareTo(BigDecimal.ZERO) > 0) {
-//                final String buyStock = orderNew.getStock();
-//                final BigDecimal buyPrice = orderNew.getPrice();
-//
-//                //filter sell orders to only ones that match the buy order
-//                List<Order> matchingSellOrders = sellOrders.stream()
-//                        .filter(order -> order.getStock().equals(buyStock)
-//                                && buyPrice.compareTo(order.getPrice()) >= 0).collect(Collectors.toList());
-//
-//                //if there is no matching order, then add to order book and end loop
-//                if(matchingSellOrders.isEmpty()){
-//                    buyOrders.add(orderNew);
-//                    break;
-//                }
-//
-//                bestSellOrder = matchingSellOrders.stream()
-//                        .min(Comparator.comparing(Order::getPrice))
-//                        .orElse(null);
-//
-//                //if(bestSellOrder != null) {
-//                    if(orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) < 0) {
-//                        trades.add(new Trade(bestSellOrder,orderNew,orderNew.getQuantity(),bestSellOrder.getPrice()));
-//                        subtractSellOrder(bestSellOrder,orderNew.getQuantity());
-//                        orderNew = subtractBuyOrder(orderNew,orderNew.getQuantity());
-//                    }
-//                    else if(orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) == 0){
-//                        trades.add(new Trade(bestSellOrder,orderNew,orderNew.getQuantity(),bestSellOrder.getPrice()));
-//                        sellOrders.remove(bestSellOrder);
-//                        orderNew = subtractBuyOrder(orderNew,orderNew.getQuantity());
-//                    }
-//                    else if(orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) > 0){
-//                        trades.add(new Trade(bestSellOrder,orderNew,bestSellOrder.getQuantity(),bestSellOrder.getPrice()));
-//                        sellOrders.remove(bestSellOrder);
-//                        orderNew = subtractBuyOrder(orderNew,bestSellOrder.getQuantity());
-//                    }
-//                //}
-//
-////                //if there is no
-////                else{
-////                    buyOrders.add(orderNew);
-////                    break;
-////                }
-//            }
-
-        }
-
-        //An example:
-
-        //Constructing a fake opposite matching order for demo purposes only
-        //In the real implementation an opposite matching order should be identified among the orders entered already
-        /*Order orderOppposite = new Order(orderNew.getClient() +
-                "_Opposite " + orderNew.getSide().getOppositeSide() + " " +
-                orderNew.getStock() + " " +
-                Environment.decimalFormat.format(orderNew.getQuantity()) + "@" +
-                Environment.decimalFormat.format(orderNew.getPrice()));
-
-        //Constructing a new trade from (orderSell, orderBuy....)
-        Trade tradeNew;
-        if (orderNew.getSide() == Side.SELL) {
-            tradeNew = new Trade(orderNew, orderOppposite, orderNew.getQuantity(), orderNew.getPrice());
-        }else{
-            tradeNew = new Trade(orderOppposite, orderNew, orderNew.getQuantity(), orderNew.getPrice());
-        }
-
-        //Adding a new trade
-        trades.add(tradeNew);*/
 
         return trades;
     }
 
     private List<Trade> makeTrades(List<Trade> trades, Order orderNew, List<Order> orderBook){
-        Order bestSellOrder;
+        Order bestMatchingOrder, sellOrder, buyOrder;
+        final String stock = orderNew.getStock();
+        final BigDecimal price = orderNew.getPrice();
+
         while(orderNew.getQuantity().compareTo(BigDecimal.ZERO) > 0) {
-            final String buyStock = orderNew.getStock();
-            final BigDecimal buyPrice = orderNew.getPrice();
 
-            //filter sell orders to only ones that match the buy order
-            List<Order> matchingSellOrders = orderBook.stream()
-                    .filter(order -> order.getStock().equals(buyStock)
-                            && buyPrice.compareTo(order.getPrice()) >= 0).collect(Collectors.toList());
+            List<Order> matchingOrders;
 
-            //if there is no matching order, then add to order book and end loop
-            if (matchingSellOrders.isEmpty()) {
-                buyOrders.add(orderNew);
-                break;
+            if(orderNew.getSide() == Side.BUY){
+                //filter sell orders to only ones that match the buy order
+                matchingOrders = orderBook.stream()
+                        .filter(order -> order.getStock().equals(stock)
+                                && price.compareTo(order.getPrice()) >= 0).collect(Collectors.toList());
+
+                if (matchingOrders.isEmpty()) {
+                    buyOrders.add(orderNew);
+                    break;
+                }
+
+                bestMatchingOrder = matchingOrders.stream()
+                        .min(Comparator.comparing(Order::getPrice))
+                        .orElse(null);
+
+                sellOrder = bestMatchingOrder;
+                buyOrder = orderNew;
+            }
+            else{
+                //filter buy orders to only ones that match the sell order
+                matchingOrders = orderBook.stream()
+                        .filter(order -> order.getStock().equals(stock)
+                                && price.compareTo(order.getPrice()) <= 0).collect(Collectors.toList());
+
+                if (matchingOrders.isEmpty()) {
+                    sellOrders.add(orderNew);
+                    break;
+                }
+
+                bestMatchingOrder = matchingOrders.stream()
+                        .max(Comparator.comparing(Order::getPrice))
+                        .orElse(null);
+
+                sellOrder = orderNew;
+                buyOrder = bestMatchingOrder;
             }
 
-            bestSellOrder = matchingSellOrders.stream()
-                    .min(Comparator.comparing(Order::getPrice))
-                    .orElse(null);
-
-            //if(bestSellOrder != null) {
-            if (orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) < 0) {
-                trades.add(new Trade(bestSellOrder, orderNew, orderNew.getQuantity(), bestSellOrder.getPrice()));
-                subtractSellOrder(bestSellOrder, orderNew.getQuantity());
-                orderNew = subtractBuyOrder(orderNew, orderNew.getQuantity());
-            } else if (orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) == 0) {
-                trades.add(new Trade(bestSellOrder, orderNew, orderNew.getQuantity(), bestSellOrder.getPrice()));
-                orderBook.remove(bestSellOrder);
-                orderNew = subtractBuyOrder(orderNew, orderNew.getQuantity());
-            } else if (orderNew.getQuantity().compareTo(bestSellOrder.getQuantity()) > 0) {
-                trades.add(new Trade(bestSellOrder, orderNew, bestSellOrder.getQuantity(), bestSellOrder.getPrice()));
-                orderBook.remove(bestSellOrder);
-                orderNew = subtractBuyOrder(orderNew, bestSellOrder.getQuantity());
+            if (buyOrder.getQuantity().compareTo(sellOrder.getQuantity()) < 0) {
+                trades.add(new Trade(sellOrder, buyOrder, buyOrder.getQuantity(), bestMatchingOrder.getPrice()));
+                subtractFromOrderBook(bestMatchingOrder, buyOrder.getQuantity(), orderBook);
+                orderNew = subtractOrder(orderNew, buyOrder.getQuantity());
+            }
+            else if (buyOrder.getQuantity().compareTo(sellOrder.getQuantity()) == 0) {
+                trades.add(new Trade(sellOrder, buyOrder, buyOrder.getQuantity(), bestMatchingOrder.getPrice()));
+                orderBook.remove(bestMatchingOrder);
+                orderNew = subtractOrder(orderNew, buyOrder.getQuantity());
+            }
+            else if (buyOrder.getQuantity().compareTo(sellOrder.getQuantity()) > 0) {
+                trades.add(new Trade(sellOrder, buyOrder, sellOrder.getQuantity(), bestMatchingOrder.getPrice()));
+                subtractFromOrderBook(bestMatchingOrder, sellOrder.getQuantity(), orderBook);
+                orderNew = subtractOrder(orderNew, sellOrder.getQuantity());
             }
         }
         return trades;
     }
 
-    private void subtractSellOrder(Order order, BigDecimal quantity){
-        int index = sellOrders.indexOf(order);
-        Order orderNew =  new Order(order.getTimeStamp() + " " +
-                order.getId() + " " +
-                order.getClient() + " " +
-                order.getSide() + " " +
-                order.getStock() + " " +
-                order.getQuantity().subtract(quantity) + "@" +
-                order.getPrice()
-        );
-        sellOrders.set(index, orderNew);
+    private void subtractFromOrderBook(Order order, BigDecimal quantity, List<Order> orderBook){
+
+        int index = orderBook.indexOf(order);
+        Order orderNew = subtractOrder(order,quantity);
+
+        if(orderNew.getQuantity().compareTo(BigDecimal.ZERO) == 0)
+            orderBook.remove(index);
+        else
+            orderBook.set(index, orderNew);
     }
 
-    private Order subtractBuyOrder(Order order, BigDecimal quantity){
+    private Order subtractOrder(Order order, BigDecimal quantity){
         return new Order(order.getTimeStamp() + " " +
                 order.getId() + " " +
                 order.getClient() + " " +
